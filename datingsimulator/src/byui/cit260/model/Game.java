3379ca78df9a62c576987ab219d94233769ec616
jalynzamora <jalynortiz@yahoo.com>
2018-05-14 13:51:19 -0600

    5/14/18 Creating Game Class

---
+++ datingsimulator/src/byui/cit260/model/Game.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author jalynzamora
+ */
+public class Game implements Serializable {
+
+    private double totalDays;
+    private double money;
+    
+    
+    public Game() {
+    }
+
+    public double getTotalDays() {
+        return totalDays;
+    }
+
+    public void setTotalDays(double totalDays) {
+        this.totalDays = totalDays;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.totalDays) ^ (Double.doubleToLongBits(this.totalDays) >>> 32));
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.money) ^ (Double.doubleToLongBits(this.money) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "totalDays=" + totalDays + ", money=" + money + '}';
+    }
+
+    
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (Double.doubleToLongBits(this.totalDays) != Double.doubleToLongBits(other.totalDays)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.money) != Double.doubleToLongBits(other.money)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
