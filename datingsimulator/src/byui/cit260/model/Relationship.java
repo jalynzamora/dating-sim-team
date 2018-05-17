8e35493a8c68da1b531ad78aab8b39688107760d
jalynzamora <jalynortiz@yahoo.com>
2018-05-17 14:10:08 -0600

    5/17/18 New classes added

---
+++ datingsimulator/src/byui/cit260/model/Relationship.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.model;
+
+/**
+ *
+ * @author jalynzamora
+ */
+public class Relationship {
+    private long relationshipStatus;
+    private String relationshipStrength;
+    private Actor actor1;
+    private Actor actor2;
+    
+    
+    
+}
