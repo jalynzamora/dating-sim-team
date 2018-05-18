2bd3a1416fbe7dac4b2f24eb1a405035f5f32947
jalynzamora <jalynortiz@yahoo.com>
2018-05-18 13:17:06 -0600

    5/18/18 Changes to map and relationship class

--- datingsimulator/src/byui/cit260/model/Map.java
+++ datingsimulator/src/byui/cit260/model/Map.java
@@ -6,6 +6,7 @@
 package byui.cit260.model;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Objects;
 
 /**
@@ -19,10 +20,109 @@ public class Map implements Serializable{
     private double currentRow;
     private double currentColumn;
     private String description;
+    private Location[][] locations;
 
     public Map() {
     }
-    
+
+    public double getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(double rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public double getColumnCount() {
+        return columnCount;
+    }
+
+    public void setColumnCount(double columnCount) {
+        this.columnCount = columnCount;
+    }
+
+    public double getCurrentRow() {
+        return currentRow;
+    }
+
+    public void setCurrentRow(double currentRow) {
+        this.currentRow = currentRow;
+    }
+
+    public double getCurrentColumn() {
+        return currentColumn;
+    }
+
+    public void setCurrentColumn(double currentColumn) {
+        this.currentColumn = currentColumn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Location[][] getLocations() {
+        return locations;
+    }
+
+    public void setLocations(Location[][] locations) {
+        this.locations = locations;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.rowCount) ^ (Double.doubleToLongBits(this.rowCount) >>> 32));
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.columnCount) ^ (Double.doubleToLongBits(this.columnCount) >>> 32));
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.currentRow) ^ (Double.doubleToLongBits(this.currentRow) >>> 32));
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.currentColumn) ^ (Double.doubleToLongBits(this.currentColumn) >>> 32));
+        hash = 79 * hash + Objects.hashCode(this.description);
+        hash = 79 * hash + Arrays.deepHashCode(this.locations);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Map{" + "rowCount=" + rowCount + ", columnCount=" + columnCount + ", currentRow=" + currentRow + ", currentColumn=" + currentColumn + ", description=" + description + ", locations=" + locations + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (Double.doubleToLongBits(this.rowCount) != Double.doubleToLongBits(other.rowCount)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.columnCount) != Double.doubleToLongBits(other.columnCount)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.currentRow) != Double.doubleToLongBits(other.currentRow)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.currentColumn) != Double.doubleToLongBits(other.currentColumn)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Arrays.deepEquals(this.locations, other.locations)) {
+            return false;
+        }
+        return true;
+    }
+
     
     }
   
--- datingsimulator/src/byui/cit260/model/Relationship.java
+++ datingsimulator/src/byui/cit260/model/Relationship.java
@@ -5,6 +5,8 @@
  */
 package byui.cit260.model;
 
+import java.util.Objects;
+
 /**
  *
  * @author jalynzamora
@@ -14,7 +16,82 @@ public class Relationship {
     private String relationshipStrength;
     private Actor actor1;
     private Actor actor2;
-    
-    
+
+    public Relationship() {
+    }
+
+    public long getRelationshipStatus() {
+        return relationshipStatus;
+    }
+
+    public void setRelationshipStatus(long relationshipStatus) {
+        this.relationshipStatus = relationshipStatus;
+    }
+
+    public String getRelationshipStrength() {
+        return relationshipStrength;
+    }
+
+    public void setRelationshipStrength(String relationshipStrength) {
+        this.relationshipStrength = relationshipStrength;
+    }
+
+    public Actor getActor1() {
+        return actor1;
+    }
+
+    public void setActor1(Actor actor1) {
+        this.actor1 = actor1;
+    }
+
+    public Actor getActor2() {
+        return actor2;
+    }
+
+    public void setActor2(Actor actor2) {
+        this.actor2 = actor2;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + (int) (this.relationshipStatus ^ (this.relationshipStatus >>> 32));
+        hash = 17 * hash + Objects.hashCode(this.relationshipStrength);
+        hash = 17 * hash + Objects.hashCode(this.actor1);
+        hash = 17 * hash + Objects.hashCode(this.actor2);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Relationship{" + "relationshipStatus=" + relationshipStatus + ", relationshipStrength=" + relationshipStrength + ", actor1=" + actor1 + ", actor2=" + actor2 + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Relationship other = (Relationship) obj;
+        if (this.relationshipStatus != other.relationshipStatus) {
+            return false;
+        }
+        if (!Objects.equals(this.relationshipStrength, other.relationshipStrength)) {
+            return false;
+        }
+        if (this.actor1 != other.actor1) {
+            return false;
+        }
+        if (this.actor2 != other.actor2) {
+            return false;
+        }
+        return true;
+    }
     
 }
