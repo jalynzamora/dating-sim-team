b1d40207100a94bd2f4b493cfe9deeda7eeaca6c
3adams15 <noahadams100@gmail.com>
2018-05-18 13:15:38 -0600

    5/18/18 completed QuestionLocation & Question

--- datingsimulator/src/byui/cit260/model/Question.java
+++ datingsimulator/src/byui/cit260/model/Question.java
@@ -6,16 +6,93 @@
 package byui.cit260.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
  * @author noahadams
  */
-public class Question implements Serializable{
-    
+public class Question implements Serializable {
+
     private String question;
     private String answer;
     private String required;
-    private String numPoints;
-    
+    private double numPoints;
+
+    public Question() {
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getRequired() {
+        return required;
+    }
+
+    public void setRequired(String required) {
+        this.required = required;
+    }
+
+    public double getNumPoints() {
+        return numPoints;
+    }
+
+    public void setNumPoints(double numPoints) {
+        this.numPoints = numPoints;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.question);
+        hash = 41 * hash + Objects.hashCode(this.answer);
+        hash = 41 * hash + Objects.hashCode(this.required);
+        hash = 41 * hash + (int) (Double.doubleToLongBits(this.numPoints) ^ (Double.doubleToLongBits(this.numPoints) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" + "question=" + question + ", answer=" + answer + ", required=" + required + ", numPoints=" + numPoints + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Question other = (Question) obj;
+        if (Double.doubleToLongBits(this.numPoints) != Double.doubleToLongBits(other.numPoints)) {
+            return false;
+        }
+        if (!Objects.equals(this.question, other.question)) {
+            return false;
+        }
+        if (!Objects.equals(this.answer, other.answer)) {
+            return false;
+        }
+        if (!Objects.equals(this.required, other.required)) {
+            return false;
+        }
+        return true;
+    }
 }
