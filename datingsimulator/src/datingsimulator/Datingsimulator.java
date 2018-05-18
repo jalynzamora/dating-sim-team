fdb847ba9cd54efc5eacf0e425d928ba54a7c78b
jalynzamora <jalynortiz@yahoo.com>
2018-05-18 14:10:11 -0600

    5/18/18 Changes to classes

--- datingsimulator/src/datingsimulator/Datingsimulator.java
+++ datingsimulator/src/datingsimulator/Datingsimulator.java
@@ -7,9 +7,12 @@ package datingsimulator;
 
 import byui.cit260.model.Actor;
 import static byui.cit260.model.Actor.Quavious;
+import static byui.cit260.model.Actor.Velma;
 import byui.cit260.model.Game;
 import byui.cit260.model.InventoryItem;
+import byui.cit260.model.Map;
 import byui.cit260.model.Player;
+import byui.cit260.model.Relationship;
 
 /**
  *
@@ -38,17 +41,36 @@ public class Datingsimulator {
         game.setPlayer(playerOne);
         
         System.out.println(game.toString());
-    
+        
         System.out.println(Actor.Quavious.toString());
         
         
-        InventoryItem itemOne = new InventoryItem();
+        InventoryItem items = new InventoryItem();
+        items.setItemType("frisbee");
+        items.setDescription("Used at connect with others at the park.");
+        items.setQuantity(4);
+        items.setGame(game);
+        
+        System.out.println(items.toString());
         
         
+        Map map = new Map();
+        map.setRowCount(2);
+        map.setColumnCount(4);
+        map.setCurrentRow(3);
+        map.setCurrentColumn(1);
+        map.setDescription("These are locations on the map");
         
-    
-    
-    
+        System.out.println(map.toString());
+        
+        
+        Relationship relationships = new Relationship();
+        relationships.setRelationshipStatus(25);
+        relationships.setRelationshipStrength(playerOneName);
+        relationships.setActor1(Velma);
+        relationships.setActor2(Quavious);
+        
+        System.out.println(relationships.toString());
 
 }
 }
