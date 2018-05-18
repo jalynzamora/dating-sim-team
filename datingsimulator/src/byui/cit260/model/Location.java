84f625e83407009b484304a3a9bcd6e3eecf2f05
3adams15 <noahadams100@gmail.com>
2018-05-18 12:49:42 -0600

    5/17/18 Created Location, Question, and QuestionLocation classes and completed Location

---
+++ datingsimulator/src/byui/cit260/model/Location.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.model;
+
+import java.awt.Point;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author noahadams
+ */
+public class Location implements Serializable {
+
+    private double row;
+    private double column;
+    private double displaySymbol;
+    private String description;
+    private Point visited;
+
+    public Location() {
+    }
+
+    public double getRow() {
+        return row;
+    }
+
+    public void setRow(double row) {
+        this.row = row;
+    }
+
+    public double getColumn() {
+        return column;
+    }
+
+    public void setColumn(double column) {
+        this.column = column;
+    }
+
+    public double getDisplaySymbol() {
+        return displaySymbol;
+    }
+
+    public void setDisplaySymbol(double displaySymbol) {
+        this.displaySymbol = displaySymbol;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Point getVisited() {
+        return visited;
+    }
+
+    public void setVisited(Point visited) {
+        this.visited = visited;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.row) ^ (Double.doubleToLongBits(this.row) >>> 32));
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.column) ^ (Double.doubleToLongBits(this.column) >>> 32));
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.displaySymbol) ^ (Double.doubleToLongBits(this.displaySymbol) >>> 32));
+        hash = 47 * hash + Objects.hashCode(this.description);
+        hash = 47 * hash + Objects.hashCode(this.visited);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "row=" + row + ", column=" + column + ", displaySymbol=" + displaySymbol + ", description=" + description + ", visited=" + visited + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (Double.doubleToLongBits(this.row) != Double.doubleToLongBits(other.row)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.column) != Double.doubleToLongBits(other.column)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.displaySymbol) != Double.doubleToLongBits(other.displaySymbol)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.visited, other.visited)) {
+            return false;
+        }
+        return true;
+    }
+}
