720618d3bf200c87c2e9888f08c7ff49dc1067d3
jalynzamora <jalynortiz@yahoo.com>
2018-06-27 14:10:16 -0600

    Merge origin/master
    Conflicts:
    	datingsimulator/src/byui/cit260/datingSim/view/GameMenuView.java

--- datingsimulator/src/byui/cit260/datingSim/view/GameControl.java
+++ datingsimulator/src/byui/cit260/datingSim/view/GameControl.java
@@ -5,7 +5,12 @@
  */
 package byui.cit260.datingSim.view;
 
+import byui.cit260.model.Actor;
+import byui.cit260.model.Game;
+import byui.cit260.model.InventoryItem;
+import byui.cit260.model.InventoryItemType;
 import byui.cit260.model.Player;
+import byui.cit260.model.Question;
 import datingsimulator.Datingsimulator;
 
 /**
@@ -24,7 +29,43 @@ class GameControl {
         return player;
     }
 
-    public static void createNewGame(Player player) {
-        System.out.println("createNewGame was called");
+    public static int createNewGame(Player player) {
+        if (player == null) {
+            return -1;
+        }
+        Game game = new Game();
+        game.setPlayer(player);
+        Datingsimulator.setCurrentGame(game);
+
+        player.setActor(Actor.Playerm);
+
+        InventoryItem[] inventoryList = createItems();
+        
+        Question[] questions = createQuestions();
+
+        return 0;
     }
+
+    private static InventoryItem[] createItems() {
+       InventoryItem[] items = new InventoryItem[3];
+       
+       InventoryItem gymClothes = new InventoryItem();
+       gymClothes.setQuantity(0);
+       gymClothes.setDescription("Clothes used for the gym.");
+       gymClothes.setItemType("Clothes");
+       items[InventoryItemType.gymClothes.ordinal()] = gymClothes;
+       
+       InventoryItem calculator = new InventoryItem("calculator", "it is pretty scratched up", 0);
+       items[InventoryItemType.calculator.ordinal()] = calculator;
+       
+       InventoryItem frisbee = new InventoryItem("frisbee", "in great condition", 0);
+       items[InventoryItemType.frisbee.ordinal()] = frisbee;
+       
+       return items;
+    }
+
+    private static Question[] createQuestions() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
