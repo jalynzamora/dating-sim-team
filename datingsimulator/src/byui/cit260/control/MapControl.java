79d7b51566152788cfad7e122b0d021baac2b2d5
jalynzamora <jalynortiz@yahoo.com>
2018-06-29 13:20:43 -0600

    Merge origin/master
    Conflicts:
    	datingsimulator/src/byui/cit260/datingSim/view/GameMenuView.java

--- datingsimulator/src/byui/cit260/control/MapControl.java
+++ datingsimulator/src/byui/cit260/control/MapControl.java
@@ -16,28 +16,29 @@ public class MapControl {
 
     public static Map createMap(Game game, int noOfRows, int noOfColumns){
     
-        if(items == null || length< 1){
-    return null;
-    }
-        if(noOfRows < 0 || noOfColumns < 0){
+        if(game == null || noOfRows < 0 || noOfColumns < 0){
             return null;
         }
         
-
-    /**
-    * // create the map
-     * object and assign values to it map = a new Map object call setter to
-     * assign value to description in map call setter to assign value to
-     * noOfRows in the map call setter to assign value to noOfColumns in map
-     * call setter to save the map in game object // create a two-dimensional
-     * array of locations and assign array to the map locations =
-     * createLocations(noOfRows, noOfColumns) if (locations is null) then return
-     * null endif call setter to save a locations array in the map object //
-     * assign actors and items to locations success =
-     * assignActorsToLocations(locations) if (success < 0) then return null
-     * endif success = assignIemsToLocations(locations) if (success < 0) then
-     * return null endif // Assign all other types objects to locations (e.g.,
-     * questions, spells) *
-     */
+        Map map = new Map();
+        
+        map.setDescription("");
+        
+        
+        
+  /***      // create the map object and assign values to it map = a new Map object
+call setter to assign value to description in map call setter to assign value to noOfRows in the map call setter to assign value to noOfColumns in map call setter to save the map in game object
+// create a two-dimensional array of locations and assign array to the map locations = createLocations(noOfRows, noOfColumns)
+if (locations is null) then
+return null endif
+call setter to save a locations array in the map object
+// assign actors and items to locations success = assignActorsToLocations(locations) if (success < 0) then
+return null endif
+success = assignIemsToLocations(locations) if (success < 0) then
+return null endif
+// Assign all other types objects to locations (e.g., questions, spells) ...
+return map
+**/
+    
 }
 }
