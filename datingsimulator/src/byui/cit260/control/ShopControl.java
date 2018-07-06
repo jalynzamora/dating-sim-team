91ff47477fbb271e0b80b5b75af17426fddcfd60
3adams15 <noahadams100@gmail.com>
2018-07-06 13:20:19 -0600

    7/06/18 created shop control class.

---
+++ datingsimulator/src/byui/cit260/control/ShopControl.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.control;
+
+/**
+ *
+ * @author noahadams
+ */
+public class ShopControl {
+    public static int[] doShopCost (int[] arr) {
+    
+      
+    }
+}
